83cb9980b666a7a8a0ce6b09f02b516b1d29efe5
Tatiana <petrenkotanya18@gmail.com>
2024-12-09 17:51:33 +0200

    4 фтв 5 tasks

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const button = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+
+  const randomColor = getRandomHexColor();
+  
+  document.body.style.backgroundColor = `rgb(${parseInt(randomColor.slice(1, 3), 16)}, ${parseInt(randomColor.slice(3, 5), 16)}, ${parseInt(randomColor.slice(5, 7), 16)})`;
+  spanColor.textContent = randomColor;
+});
